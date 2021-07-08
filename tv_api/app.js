8fa0badb8ad6d82b666ea1f91943aa464cc8099b
edzaans-git <56066840+edzaans-git@users.noreply.github.com>
2021-07-08 19:31:40 +0100

    Update app.js
    Template for future use

--- tv_api/app.js
+++ tv_api/app.js
@@ -4,6 +4,25 @@ form.addEventListener('submit', async function (e) {
     console.log('Submitted');
     const searchValue = form.elements.query.value;
     console.log(searchValue);
-    const response = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchValue}`);
-    console.log(response.data);
+    const config = { params: { q: searchValue } };
+    const response = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+    //console.log();
+    createImages(response.data);
+    form.elements.query.value = '';
+
 });
+
+const createImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            //console.log(result);
+            const img = document.createElement('img');
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        } else {
+
+            console.log('No Image for this show');
+        }
+
+    };
+};
